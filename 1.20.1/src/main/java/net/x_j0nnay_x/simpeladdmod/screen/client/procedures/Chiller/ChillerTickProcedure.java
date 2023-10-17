3cf4d0262081b1a4d167e5d7f57cf9d3ff3cfd15
x-j0nnay-x <x.j0nnay.x@gmail.com>
2023-10-17 16:37:29 -0700

    fixes chiller progress bar once full

--- 1.20.1/src/main/java/net/x_j0nnay_x/simpeladdmod/screen/client/procedures/Chiller/ChillerTickProcedure.java
+++ 1.20.1/src/main/java/net/x_j0nnay_x/simpeladdmod/screen/client/procedures/Chiller/ChillerTickProcedure.java
@@ -433,7 +433,6 @@ public class ChillerTickProcedure {
 		}
 
 //Run
-
 		if ((new Object() {
 			public double getValue(LevelAccessor world, BlockPos pos, String tag) {
 				BlockEntity blockEntity = world.getBlockEntity(pos);
@@ -455,7 +454,15 @@ public class ChillerTickProcedure {
 					return blockEntity.getPersistentData().getDouble(tag);
 				return -1;
 			}
-		}.getValue(world, BlockPos.containing(x, y, z), "SnowLevel") > 0) {
+		}.getValue(world, BlockPos.containing(x, y, z), "SnowLevel") > 0 && new Object() {
+			public int getAmount(LevelAccessor world, BlockPos pos, int slotid) {
+				AtomicInteger _retval = new AtomicInteger(0);
+				BlockEntity _ent = world.getBlockEntity(pos);
+				if (_ent != null)
+					_ent.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> _retval.set(capability.getStackInSlot(slotid).getCount()));
+				return _retval.get();
+			}
+		}.getAmount(world, BlockPos.containing(x, y, z), 2) <= 63) {
 			if (!world.isClientSide()) {
 				BlockPos _bp = BlockPos.containing(x, y, z);
 				BlockEntity _blockEntity = world.getBlockEntity(_bp);
