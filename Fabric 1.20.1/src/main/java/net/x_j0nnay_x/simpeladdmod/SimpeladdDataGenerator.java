b72327039923296dd46c73ad914374ec4b318015
x-j0nnay-x <x.j0nnay.x@gmail.com>
2023-11-13 10:45:26 -0800

    working on fabric port

---
+++ Fabric 1.20.1/src/main/java/net/x_j0nnay_x/simpeladdmod/SimpeladdDataGenerator.java
@@ -0,0 +1,11 @@
+package net.x_j0nnay_x.simpeladdmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpeladdDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
