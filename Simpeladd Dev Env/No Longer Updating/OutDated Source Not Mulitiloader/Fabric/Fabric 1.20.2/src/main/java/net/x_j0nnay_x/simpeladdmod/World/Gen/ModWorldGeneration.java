3f1088bbd1286d24b94878a4c08f25153b264293
x-j0nnay-x <x.j0nnay.x@gmail.com>
2024-10-31 15:05:09 -0700

    Fixing GitHub

--- Simpeladd Dev Env/No Longer Updating/OutDated Source Not Mulitiloader/Fabric/Fabric 1.20.2/src/main/java/net/x_j0nnay_x/simpeladdmod/World/Gen/ModWorldGeneration.java
+++
@@ -1,7 +0,0 @@
-package net.x_j0nnay_x.simpeladdmod.World.Gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen() {
-        ModOreGeneration.generateOres();
-    }
-}
