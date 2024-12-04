82a9bccbed46adb6b0378a802d7ea39a86020c06
x-j0nnay-x <x.j0nnay.x@gmail.com>
2024-12-04 14:35:03 -0800

    1.20.1-V1.133-B-003-Build
    this just adds some small changes to the home want and block textures this is the last update for this version of the mod.

--- common/src/main/java/net/x_j0nnay_x/simpeladd/item/HomeWand.java
+++ common/src/main/java/net/x_j0nnay_x/simpeladd/item/HomeWand.java
@@ -1,26 +1,24 @@
 package net.x_j0nnay_x.simpeladd.item;
 
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.List;
 
 public class HomeWand extends Item {
 
     private int cooldown;
     private int maxcooldown = 240;
+    private boolean oncooldown;
+    private boolean homeSet;
     private int X;
     private int Y;
     private int Z;
@@ -31,19 +29,16 @@ public class HomeWand extends Item {
 
     @Override
     public void inventoryTick(ItemStack $$0, Level $$1, Entity $$2, int $$3, boolean $$4) {
-        setIntValues($$0);
-        if($$0.getOrCreateTag().getBoolean("oncooldown") == true) {
-            this.cooldown ++;
-            if(this.cooldown == 10) {
-                $$0.getOrCreateTag().putBoolean("woosh", false);
-            }
-            if(this.cooldown >= this.maxcooldown) {
-                $$0.getOrCreateTag().putBoolean("oncooldown", false);
-                this.cooldown = 0;
+        if(!$$1.isClientSide) {
+            if (this.oncooldown) {
+                this.cooldown++;
+                if (this.cooldown >= this.maxcooldown) {
+                    this.oncooldown = false;
+                    this.cooldown = 0;
+                }
             }
         }
-
-        super.inventoryTick($$0, $$1, $$2, $$3, $$4);
+            super.inventoryTick($$0, $$1, $$2, $$3, $$4);
     }
     private void setIntValues(ItemStack itemStack){
         this.X = (int) itemStack.getOrCreateTag().getDouble("homeX");
@@ -52,44 +47,43 @@ public class HomeWand extends Item {
     }
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack itemStack  = player.getItemInHand(hand);
-        if(player.isCrouching()) {
-            if(!itemStack.getOrCreateTag().getBoolean("homeSet")) {
-                setHome(itemStack, player);
-                player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeset"), true);
-            }else{
-                clearHome(itemStack);
-                player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeclear"), true);
+        if(!level.isClientSide) {
+            ItemStack itemStack = player.getItemInHand(hand);
+            if (player.isCrouching()) {
+                if (!this.homeSet) {
+                    setHome(itemStack, player);
+                    player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeset"), true);
+                } else {
+                    clearHome(itemStack);
+                    player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeclear"), true);
+                }
+            } else {
+                if(this.homeSet){
+                tryTellaport(player, itemStack);}
+                else {
+                    player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeless"), true);
+                }
             }
-        }else {
-            tryTellaport(player, itemStack);
         }
-        return super.use(level, player, hand);
+            return super.use(level, player, hand);
     }
 
     public void tryTellaport(Player player, ItemStack itemStack) {
-        if (!itemStack.getOrCreateTag().getBoolean("oncooldown") && itemStack.getOrCreateTag().getBoolean("homeSet")) {
-            itemStack.getOrCreateTag().putBoolean("oncooldown", true);
-            itemStack.getOrCreateTag().putBoolean("woosh", true);
+        if (!this.oncooldown) {
+            this.oncooldown = true;
             player.playSound(SoundEvents.ENDERMAN_TELEPORT, 1.0F, 1.0F);
             player.teleportTo(itemStack.getOrCreateTag().getDouble("homeX"), itemStack.getOrCreateTag().getDouble("homeY"), itemStack.getOrCreateTag().getDouble("homeZ"));
             player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.use"), true);
-        } if(itemStack.getOrCreateTag().getBoolean("oncooldown") && !itemStack.getOrCreateTag().getBoolean("woosh"))
-            if (!itemStack.getOrCreateTag().getBoolean("homeSet")) {
-                player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeless"), true);
-            } else {
-                player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.cooldown"), true);
-            }
-            if (!itemStack.getOrCreateTag().getBoolean("homeSet")) {
-            player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.homeless"), true);
-            }
-        return;
+        }
+        if (this.oncooldown) {
+            player.displayClientMessage(Component.translatable("item.simpeladdmod.homewand.cooldown"), true);
+        }
     }
 
 
 
     public void clearHome(ItemStack itemStack) {
-        itemStack.getOrCreateTag().putBoolean("homeSet", false);
+        this.homeSet = false;
         itemStack.getOrCreateTag().remove("homeX");
         itemStack.getOrCreateTag().remove("homeY");
         itemStack.getOrCreateTag().remove("homeZ");
@@ -102,17 +96,17 @@ public class HomeWand extends Item {
             itemStack.getOrCreateTag().putDouble("homeX", player.getX());
             itemStack.getOrCreateTag().putDouble("homeY", player.getY());
             itemStack.getOrCreateTag().putDouble("homeZ", player.getZ());
-            itemStack.getOrCreateTag().putBoolean("homeSet", true);
-
+            this.homeSet = true;
+            setIntValues(itemStack);
     }
 
 
     @Override
     public void appendHoverText(ItemStack $$0, @Nullable Level $$1, List<Component> $$2, TooltipFlag $$3) {
-        if($$0.getOrCreateTag().getBoolean("homeSet") == true) {
+        if(this.homeSet) {
             String title = Component.translatable("item.simpeladdmod.homewand.homelocal").getString();
             $$2.add(Component.literal(title +"X " + this.X + " : Y " + this.Y + " : Z " + this.Z));
-            if($$0.getOrCreateTag().getBoolean("oncooldown") == true) {
+            if(this.oncooldown) {
                 $$2.add(Component.translatable("item.simpeladdmod.homewand.cooldown"));
             }
         } else {
